630d771d1ad26eb5447aba04ff4a81be2a0e5aa6
zetahawke <michel.szinavel@gmail.com>
2016-07-10 23:23:04 -0400

    fixing error with the Picturable module in concerns under models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  include Picturable
+  #include Picturable
   include PublicActivity::Model
   tracked owner: Proc.new{ |controller, model| controller.current_user }
   
@@ -12,6 +12,43 @@ class Post < ActiveRecord::Base
 
   before_save :default_values
 
+  after_save :save_image
+
+  #if self.respond_to?(:name)
+  FILE_PATH = File.join Rails.root, "public", "files" #, "attachments"
+  #else
+  # FILE_PATH = File.join Rails.root, "public", "files", "posts"
+  #end
+  
+
+  def file=(file)
+    unless file.blank?
+      @file = file
+      if self.respond_to?(:name)
+        self.name = file.original_filename.split(".").first.downcase
+      end
+      self.extension = file.original_filename.split(".").last.downcase
+    end
+  end
+
+  def image_path
+    File.join FILE_PATH, "#{self.id}.#{self.extension}"
+  end
+
+  def had_file?
+    File.exists? image_path
+  end
+
+  private
+  def save_image
+    if @file
+      FileUtils.mkdir_p FILE_PATH
+      File.open(image_path, "wb") do |f|
+        f.write(@file.read)
+      end
+      @file = nil
+    end
+  end
 
 
   def default_values

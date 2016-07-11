26108e989f0b3190293b0bb935dd19bf27f31c81
zetahawke <michel.szinavel@gmail.com>
2016-07-10 22:33:07 -0400

    deploying with postgres

--- db/migrate/20160707195508_create_friendships.rb
+++ db/migrate/20160707195508_create_friendships.rb
@@ -2,7 +2,7 @@ class CreateFriendships < ActiveRecord::Migration
   def change
     create_table :friendships do |t|
       t.references :user, index: true, foreign_key: true
-      t.references :friend, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true, as: :friend
 
       t.timestamps null: false
     end
